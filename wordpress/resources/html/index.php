4856c4228a93d39798ff644ed36305158bb4b2cc
Owen Parkins <oparkins@gmail.com>
2019-04-23 16:39:38 -0600

    Added wordpress site

---
+++ wordpress/resources/html/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo ('<html>
+<head>
+    <title>Testing Website</title>
+</head>
+<body>
+    <h1>This is a super cheesy website, and it\'s under development!</h1>
+    <p>Click <a href="./form.php">here</a> to use our awesome form!</p>
+    <p>Stay tuned for updates!</p>
+</body>
+</html>
+');
+
+?>
